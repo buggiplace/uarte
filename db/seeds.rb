1e3bc605603356ba18ab50b0278ae02a08fe90ce
Moritz Bugiel <mc.bugiel@googlemail.com>
2020-12-08 13:40:00 +0100

    started with artwork show

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,7 +31,6 @@ uarte = Artist.create!(first_name: 'Klaus',
 
 puts 'Create Artworks'
 artwork1 = Artwork.create!(title: 'Rizzy',
-                     image_url: 'http://via.placeholder.com/300',
                      description: 'Something nice in a hand',
                      artist_id: uarte.id,
                      artwork_type: "Wood"
@@ -43,7 +42,6 @@ artwork1.photo.attach(io: file01, filename: '01-rizzi.jpg', content_type: 'image
 
 
 artwork2 = Artwork.create!(title: 'Rizzi',
-                     image_url: 'http://via.placeholder.com/300',
                      description: 'Some red, some yellow, some blue',
                      artist_id: uarte.id,
                      artwork_type: "Acryl"
@@ -53,7 +51,6 @@ file02 = URI.open('https://res.cloudinary.com/dvoduvlcz/image/upload/v1607414208
 artwork2.photo.attach(io: file02, filename: '02-Untitled', content_type: 'image/jpg')
 
 artwork3 = Artwork.create!(title: 'Untitled',
-                     image_url: 'http://via.placeholder.com/300',
                      description: 'A colorful picture',
                      artist_id: uarte.id,
                      artwork_type: "Color"
@@ -63,7 +60,6 @@ file03 = URI.open('https://res.cloudinary.com/dvoduvlcz/image/upload/v1607414210
 artwork3.photo.attach(io: file03, filename: '03-Gold.jpg', content_type: 'image/jpg')
 
 artwork4 = Artwork.create!(title: 'Gold',
-                     image_url: 'http://via.placeholder.com/300',
                      description: 'A beautiful chair',
                      artist_id: uarte.id,
                      artwork_type: "Furniture"
