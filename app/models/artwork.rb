4533c4cbe99d936593300a0e72d483821ab2b38c
Moritz Bugiel <mc.bugiel@googlemail.com>
2020-12-08 09:41:53 +0100

    pic on artwork show

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,4 +1,4 @@
 class Artwork < ApplicationRecord
   belongs_to :artist
-  has_one_attached :photo
+  has_many_attached :photos
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -30,12 +30,18 @@ uarte = Artist.create!(first_name: 'Klais',
 
 
 puts 'Create Artworks'
-artwork1 = Artwork.create!(title: 'HuiBu',
+artwork1 = Artwork.create!(title: 'Rizzy',
                      image_url: 'http://via.placeholder.com/300',
                      description: 'Something nice in a hand',
                      artist_id: uarte.id,
                      artwork_type: "Wood"
                      )
+
+# file01 = Rails.root.join('images', '01-Rizzi.jpg')
+# artwork1.photos.attach(io: File.open(file01), filename: "01-Rizzi.jpg", content_type: "image/jpg")
+
+file01 = URI.open('https://res.cloudinary.com/dvoduvlcz/image/upload/v1607414213/Uarte/01-Rizzi_fgoaqr.jpg')
+artwork1.photos.attach(io: file01, filename: '01-rizzi.jpg', content_type: 'image/jpg')
 
 artwork2 = Artwork.create!(title: 'HuiBu2',
                      image_url: 'http://via.placeholder.com/300',
@@ -57,5 +63,9 @@ artwork4 = Artwork.create!(title: 'HuiBu4',
                      artist_id: uarte.id,
                      artwork_type: "Furniture"
                      )
+
+
+
+
 
 puts "artworks created"
