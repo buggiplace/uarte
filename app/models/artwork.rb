28dc806b4f0c2eb6dac5dbd4247a5705276d8051
Moritz Bugiel <mc.bugiel@googlemail.com>
2020-12-11 10:08:10 +0100

    added previous and next artwork feature

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -2,4 +2,19 @@ class Artwork < ApplicationRecord
   belongs_to :artist
   has_one_attached :photo
 
+  # def previous_artwork
+  #   self.class.first(:conditions => ["title < ?", title], :order => "title desc")
+  # end
+
+  # def next_artwork
+  #   self.class.first(:conditions => ["title > ?", title], :order => "title asc")
+  # end
+
+  def previous
+    Artwork.where(["id < ?", id]).last
+  end
+
+  def next
+    Artwork.where(["id > ?", id]).first
+  end
 end
